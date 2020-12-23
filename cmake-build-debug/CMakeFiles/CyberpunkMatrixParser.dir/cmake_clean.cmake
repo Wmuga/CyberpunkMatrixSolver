b4104a343af3c74d7ef83671eb7415f5a82237c8
Wmuga <CherkasovAA@mpei.ru>
2020-12-23 15:58:17 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/CyberpunkMatrixParser.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CyberpunkMatrixParser.dir/main.cpp.obj"
+  "CyberpunkMatrixParser.exe"
+  "CyberpunkMatrixParser.exe.manifest"
+  "CyberpunkMatrixParser.pdb"
+  "libCyberpunkMatrixParser.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CyberpunkMatrixParser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <ctime>
+using namespace std;
+
+string* matrix;
+int matrix_size, buffer_size;
+vector<string> sequences;
+bool foundBest =false;
+int best_score = 0;
+string best_line;
+vector<pair<int,int>> path;
+
+void get_data(){
+    cout <<"Matrix:\n";
+    FILE* filein = fopen("in.txt","r");
+    for (int line=0;line<matrix_size;line++){
+        char buffer[matrix_size*2+1];
+        fread(buffer,1,matrix_size*2+1,filein);
+        for (int byte=0;byte<matrix_size;byte++){
+            (matrix)[line*6+byte]=buffer[byte*2];
+            (matrix)[line*6+byte]+=buffer[byte*2+1];
+            cout << (matrix)[line*6+byte] << " ";
+        }
+        cout << endl;
+    }
+    cout << "Sequences\n";
+    char buffer[256];
+    while(fscanf(filein,"%s",buffer)!=EOF){
+        sequences.emplace_back(buffer);
+        cout << buffer << endl;
+    }
+}
+
+bool ContainsCoords(const vector<pair<int,int>>& thisPath,const pair<int,int>& coord){
+    for (auto c: thisPath){
+        if (c.first==coord.first && c.second==coord.second) return true;
+    }
+    return false;
+}
+
+//55 55 55 bd 7a 55 e9 7a
+void get_possible_sequences(int& index_x, int index_y, vector<pair<int,int>> thisPath, string thisStr="", bool xy=true){
+    if (foundBest) return;
+    if (!thisStr.empty() && thisStr.length()/2==buffer_size) {
+        int isSeq = 0;
+        for (const auto& seq: sequences){
+            if (thisStr.find(seq)!=string::npos) isSeq++;
+        }
+        if (isSeq>best_score) {
+            best_score=isSeq;
+            path = thisPath;
+            best_line=thisStr;
+        }
+        if (isSeq==sequences.size()){
+            foundBest=true;
+        }
+    }
+    else{
+        thisStr+=matrix[index_y*matrix_size+index_x];
+        thisPath.emplace_back(index_x, index_y);
+        for (int i=0;i<matrix_size;i++){
+            if ( xy && !ContainsCoords(thisPath,make_pair(index_x,i))) get_possible_sequences(index_x, i, thisPath, thisStr, !xy);
+            if (!xy && !ContainsCoords(thisPath,make_pair(i,index_y))) get_possible_sequences(i, index_y, thisPath, thisStr, !xy);
+        }
+    }
+}
+
+void set_search(int start_index,int end_index){
+    for (int i=start_index;i<=end_index;i++){
+        get_possible_sequences(i,0,vector<pair<int,int>>());
+    }
+}
+
+void get_possible_sequences_start(){
+    set<string> s1,s2,s3;
+    //Огрызок для поиска в нескольких потоках
+    set_search(0,matrix_size/3);
+    set_search(matrix_size/3+1,matrix_size/3*2);
+    set_search(matrix_size/3*2+1,matrix_size-1);
+
+}
+
+int main() {
+    cout << "Type matrix size and buffer size\n";
+    cin>>matrix_size>>buffer_size;
+    matrix =  new string[matrix_size*matrix_size];
+    get_data();
+    auto start = clock();
+    get_possible_sequences_start();
+    cout << "Done\nElapsed time = " << (clock()-start) <<" clock ticks" << endl;
+    cout << "Lined up sequences: " << best_score << endl << "Line: ";
+    for (int i=0;i<best_line.size()/2;i++) cout << best_line[i*2] << best_line[i*2+1] << " ";
+    cout << endl << "path:";
+    for (auto c: path) cout << "{ x:" << c.first << " y:" << c.second <<" } ";
+    delete[]matrix;
+    return 0;
+}
